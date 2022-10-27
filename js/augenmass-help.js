6f5f9774094623e0bab735de0cc72c2edc125e13
Jannis Schoenleber <janniss@student-net-vpn-0699.intern.ethz.ch>
2022-10-27 16:25:51 +0200

    style(prettier): format `js` with `prettier`

--- js/augenmass-help.js
+++ js/augenmass-help.js
@@ -2,57 +2,60 @@
 // performs, the next level is shown. Once the user managed all of them,
 // we're fading into silency.
 var HelpLevelEnum = {
-    DONE_FILE_LOADING:  0,
-    DONE_START_LINE:    1,
-    DONE_FINISH_LINE:   2,
-    DONE_ADD_ANGLE:     3,
-    DONE_SET_LEN:       4,
+  DONE_FILE_LOADING: 0,
+  DONE_START_LINE: 1,
+  DONE_FINISH_LINE: 2,
+  DONE_ADD_ANGLE: 3,
+  DONE_SET_LEN: 4,
 };
 
 // Constructor function.
 function HelpSystem(helptext_span) {
-    this.last_help_level_ = -1;
+  this.last_help_level_ = -1;
 
-    // Initial text.
-    printHelp("(Only your browser reads the image. It is not uploaded anywhere.)");
+  // Initial text.
+  printHelp(
+    "(Only your browser reads the image. It is not uploaded anywhere.)"
+  );
 
-    this.achievementUnlocked = function(level) {
-	if (level < this.last_help_level_)
-	    return;
-	this.last_help_level_ = level;
-	printLevel(level);
+  this.achievementUnlocked = function (level) {
+    if (level < this.last_help_level_) return;
+    this.last_help_level_ = level;
+    printLevel(level);
+  };
+
+  function printLevel(requested_level) {
+    switch (requested_level) {
+      case HelpLevelEnum.DONE_FILE_LOADING:
+        printHelp("Click somewhere to start a line.");
+        break;
+      case HelpLevelEnum.DONE_START_LINE:
+        printHelp("A second click finishes the line. Or Cancel with 'Esc'.");
+        break;
+      case HelpLevelEnum.DONE_FINISH_LINE:
+        printHelp(
+          "TIP: starting a line where another ends measures their angles."
+        );
+        break;
+      case HelpLevelEnum.DONE_ADD_ANGLE:
+        printHelp("Double click on center of line to set relative size.");
+        break;
+      case HelpLevelEnum.DONE_SET_LEN:
+        printHelp("Congratulations - you are an expert now!", true);
+        break;
     }
+  }
 
-    function printLevel(requested_level) {
-	switch (requested_level) {
-	case HelpLevelEnum.DONE_FILE_LOADING:
-	    printHelp("Click somewhere to start a line.");
-	    break;
-	case HelpLevelEnum.DONE_START_LINE:
-	    printHelp("A second click finishes the line. Or Cancel with 'Esc'.");
-	    break;
-	case HelpLevelEnum.DONE_FINISH_LINE:
-	    printHelp("TIP: starting a line where another ends measures their angles.");
-	    break;
-	case HelpLevelEnum.DONE_ADD_ANGLE:
-	    printHelp("Double click on center of line to set relative size.");
-	    break;
-	case HelpLevelEnum.DONE_SET_LEN:
-	    printHelp("Congratulations - you are an expert now!", true);
-	    break;
-	}
+  function printHelp(help_text, fade_away) {
+    if (help_text == undefined) return;
+    while (helptext_span.firstChild) {
+      helptext_span.removeChild(helptext.firstChild);
     }
+    helptext_span.appendChild(document.createTextNode(help_text));
 
-    function printHelp(help_text, fade_away) {
-	if (help_text == undefined) return;
-	while (helptext_span.firstChild) {
-	    helptext_span.removeChild(helptext.firstChild);
-	}
-	helptext_span.appendChild(document.createTextNode(help_text));
-	
-	if (fade_away != undefined && fade_away) {
-	    helptext_span.style.transition = "opacity 10s";
-	    helptext_span.style.opacity = 0;
-	}
+    if (fade_away != undefined && fade_away) {
+      helptext_span.style.transition = "opacity 10s";
+      helptext_span.style.opacity = 0;
     }
+  }
 }
